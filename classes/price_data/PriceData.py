4281fe9f6db8e50184c3bda8372c2c62e2bd64b1
eustin <eustin.jvans@gmail.com>
2019-05-13 18:03:43 +1000

    get different aspects of price data

--- classes/price_data/PriceData.py
+++ classes/price_data/PriceData.py
@@ -17,7 +17,6 @@ class PriceData:
         self.data = pandas_gbq.read_gbq(sql, project_id=os.environ['BQ_PROJECT'], credentials=credentials)
         self._print_data_shape()
 
-
     def fetch_table(self, table_name):
         query = "select * from {TABLE}".format(TABLE=table_name)
         self.data = pandas_gbq.read_gbq(query, project_id=os.environ['BQ_PROJECT'], credentials=credentials)
@@ -29,18 +28,24 @@ class PriceData:
 
     # return different aspects of price
     def open(self):
-        pass
+        self._data_exists()
+        return self.data.loc[:, 'open']
 
     def high(self):
-        pass
+        self._data_exists()
+        return self.data.loc[:, 'high']
 
     def low(self):
-        pass
+        self._data_exists()
+        return self.data.loc[:, 'low']
 
     def close(self):
-        pass
-
+        self._data_exists()
+        return self.data.loc[:, 'close']
 
     def _print_data_shape(self):
         print(self.data.shape)
 
+    def _data_exists(self):
+        if self.data is None:
+            raise ValueError("no data! get it first")
