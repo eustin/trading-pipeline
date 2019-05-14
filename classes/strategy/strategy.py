267fae76590af47510baebad68c0d4519a255033
eustin <eustin.jvans@gmail.com>
2019-05-15 06:04:03 +1000

    rename files to lowercase

--- classes/strategy/strategy.py
+++ classes/strategy/strategy.py
@@ -10,13 +10,18 @@
 # maybe signal is the indicator on which long and short rules are based
 
 class Strategy:
+
     def __init__(self, price_data, signal):
         self.price_data = price_data
         self.signal = signal
+        self.rules = {'long': None, 'short': None}
 
-    def long(self):
+
+    def _long(self):
         pass
 
-    def short(self):
+    def _short(self):
         pass
 
+    def set_rules(self, direction, func):
+        self.rules[direction] = func
